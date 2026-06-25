53840a585fe0e0d097b4eaccf251f64d403a4b1a
Satya Nagesh <hello@satyanagesh.dev>
2026-06-25 04:26:23 -0500

    Dome gallery minRadius 750px

--- js/dome-gallery.js
+++ js/dome-gallery.js
@@ -647,7 +647,7 @@ function initProjectDome(projectData) {
     grayscale: true,
     maxVerticalRotationDeg: 20,
     dragDampening: 3.6,
-    minRadius: 400,
+    minRadius: 750,
     openedImageWidth: '420px',
     openedImageHeight: '340px',
   });
