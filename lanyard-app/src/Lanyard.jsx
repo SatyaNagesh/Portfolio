409d33156c548ee1655e9b6b0c2752821519a667
Satya Nagesh <hello@satyanagesh.dev>
2026-06-25 03:07:23 -0500

    Lower anchor to y=5 so card is fully visible

--- lanyard-app/src/Lanyard.jsx
+++ lanyard-app/src/Lanyard.jsx
@@ -216,7 +216,7 @@ function Band({
 
   return (
     <>
-      <group position={[4, 8, 0]}>
+      <group position={[4, 5, 0]}>
         <RigidBody ref={fixed} {...segmentProps} type="fixed" />
         <RigidBody position={[0.5, 0, 0]} ref={j1} {...segmentProps}>
           <BallCollider args={[0.1]} />
