799609b15ec1e46b9b127c82a34cf4e8435e72d9
Satya Nagesh <hello@satyanagesh.dev>
2026-06-25 02:51:29 -0500

    Integrate exact React Bits Lanyard component with imported assets

--- lanyard-app/src/main.jsx
+++ lanyard-app/src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Lanyard from './Lanyard'
+import profile from './profile.png'
 import './Lanyard.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Lanyard
     position={[0, 0, 30]}
     gravity={[0, -40, 0]}
-    frontImage="./profile.png"
+    frontImage={profile}
   />
 )
