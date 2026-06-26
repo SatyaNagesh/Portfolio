b7b0e4ac9b6d5b0699cd2f8a439b23ff3a833351
R. Satya Nagesh <satyanagesh.r@gmail.com>
2026-06-26 02:06:07 -0500

    Increase grid size from 60 to 200

--- pixeltrail-app/src/main.jsx
+++ pixeltrail-app/src/main.jsx
@@ -4,7 +4,7 @@ import './PixelTrail.css'
 
 <div style={{ height: '500px', position: 'relative', overflow: 'hidden'}}>
     <PixelTrail
-      gridSize={60}
+      gridSize={200}
       trailSize={0.1}
       maxAge={400}
       interpolate={2.5}
